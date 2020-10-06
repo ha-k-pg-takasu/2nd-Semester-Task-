9f175a7f7898298b063f9137efa78718cfb70f2c
MSI\高須 <pixel157964@gmail.com>
2020-10-06 16:06:28 +0900

    リファクタリングその３ ・unitに宣言していた変数を、構造体に変更

--- 01_1006/classpractic/Unit.cpp
+++ 01_1006/classpractic/Unit.cpp
@@ -3,15 +3,14 @@
 
 Unit::Unit()
 {
-
-	m_hp = 0;
-	m_str = 0;
-	m_mag = 0;
-	m_tec = 0;
-	m_spd = 0;
-	m_def = 0;
-	m_luc = 0;
-	m_mde = 0;
+	m_status._hp = 0;
+	m_status._str = 0;
+	m_status._mag = 0;
+	m_status._tec = 0;
+	m_status._spd = 0;
+	m_status._def = 0;
+	m_status._luc = 0;
+	m_status._mde = 0;
 }
 
 Unit::~Unit()
@@ -21,15 +20,13 @@ Unit::~Unit()
 
 void Unit::Printstatus()
 {
-
-
-	printf("HP  %d\n", m_hp);
-	printf("STR %d\n", m_str);
-	printf("MAG %d\n", m_mag);
-	printf("TEC %d\n", m_tec);
-	printf("SPD %d\n", m_spd);
-	printf("DEF %d\n", m_def);
-	printf("LUC %d\n", m_luc);
-	printf("MDE %d\n", m_mde);
+	printf("HP  %d\n", m_status._hp);
+	printf("STR %d\n", m_status._str);
+	printf("MAG %d\n", m_status._mag);
+	printf("TEC %d\n", m_status._tec);
+	printf("SPD %d\n", m_status._spd);
+	printf("DEF %d\n", m_status._def);
+	printf("LUC %d\n", m_status._luc);
+	printf("MDE %d\n", m_status._mde);
 }
 
--- 01_1006/classpractic/Unit.h
+++ 01_1006/classpractic/Unit.h
@@ -1,8 +1,12 @@
 #ifndef UNIT_H
 #define UNIT_H
+
+#include"Definition.h"
+
 class Unit
 {
 public:
+
 	//コンストラクタ
 	Unit();
 	//デストラクタ
@@ -11,15 +15,8 @@ public:
 	void Printstatus();
 
 private:
-	int m_hp;
-	int m_str;
-	int m_mag;
-	int m_tec;
-	int m_spd;
-	int m_def;
-	int m_luc;
-	int m_mde;
 
+	status m_status;
 
 };
 #endif
