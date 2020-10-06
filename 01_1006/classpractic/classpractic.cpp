3d761c81139f15ec402fbd3ee740fa4c003b6a28
MSI\高須 <pixel157964@gmail.com>
2020-10-06 14:03:23 +0900

    リファクタリング前

--- 01_1006/classpractic/classpractic.cpp
+++ 01_1006/classpractic/classpractic.cpp
@@ -18,11 +18,20 @@ public:
 	int m_def;
 	int m_luc;
 	int m_mde;
+
+
 };
 
 Fighter::Fighter()
 {
-
+	m_hp = 0;
+	m_str = 0;
+	m_mag = 0;
+	m_tec = 0;
+	m_spd = 0;
+	m_def = 0;
+	m_luc = 0;
+	m_mde = 0;
 }
 
 Fighter::~Fighter()
@@ -32,6 +41,16 @@ Fighter::~Fighter()
 
 int main()
 {
+	Fighter fighter;
+
+	printf("HP %d\n",fighter.m_hp);
+	printf("HP %d\n", fighter.m_str);
+	printf("HP %d\n", fighter.m_mag);
+	printf("HP %d\n", fighter.m_tec);
+	printf("HP %d\n", fighter.m_spd);
+	printf("HP %d\n", fighter.m_def);
+	printf("HP %d\n", fighter.m_luc);
+	printf("HP %d\n", fighter.m_mde);
 
 	system("pause");
 	return 0;
